85eabef7b330fcd425bd06a09a41c0d64decad6e
atuljd <ajdjava@gmail.com>
2019-04-16 16:10:06 +0530

    add controller and view

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-    // test changes
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
